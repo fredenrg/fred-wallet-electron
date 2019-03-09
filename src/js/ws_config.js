c3571c9479efce0a8d80b984b4e5782bd6150409
fredenrg <39649555+fredenrg@users.noreply.github.com>
2019-03-09 21:24:32 +0400

    port change

--- src/js/ws_config.js
+++ src/js/ws_config.js
@@ -38,8 +38,8 @@ config.remoteNodeListUpdateUrl = 'null';
 
 // fallback remote node list, in case fetching update failed, fill this with known to works remote nodes
 config.remoteNodeListFallback = [
-    'pnode.fredcoinpool.com:8070',
-    'pnode2.fredcoinpool.com:8070',
+    'pnode.fredcoinpool.com:32809',
+    'pnode2.fredcoinpool.com:32809',
     '127.0.0.1:8070',
 ];
 
